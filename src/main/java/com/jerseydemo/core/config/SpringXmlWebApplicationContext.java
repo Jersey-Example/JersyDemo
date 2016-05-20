2132f1c571dcf7ac32d277641cc05ff40c7383ae
lian819 <lianxinzhong@126.com>
2016-05-20 17:44:13 +0800

    modify code style

--- src/main/java/com/jerseydemo/core/config/SpringXmlWebApplicationContext.java
+++ src/main/java/com/jerseydemo/core/config/SpringXmlWebApplicationContext.java
@@ -5,10 +5,10 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
 import com.jerseydemo.core.utilities.ConfigLocationHelper;
 
 public class SpringXmlWebApplicationContext extends XmlWebApplicationContext {
+
 	public String[] getDefaultConfigLocations() {
 		System.out.println("[%SpringXmlWebApplicationContext.getDefaultConfigLocations() begin%]");
-		String[] returnStringArray = ConfigLocationHelper.getConfigLocationArray("spring",
-				this.getServletContext().getRealPath(""));
+		String[] returnStringArray = ConfigLocationHelper.getConfigLocationArray("spring", this.getServletContext().getRealPath(""));
 		for (int i = 0; i < returnStringArray.length; i++) {
 			System.out.println(returnStringArray[i]);
 		}
